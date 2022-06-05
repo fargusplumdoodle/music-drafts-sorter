5830de12f5d88beb6d060cf67e7591370998f2f1
fargus plumdoodle <fargusplumdoodle@gmail.com>
2022-06-04 22:27:14 -0700

    working

--- src/main.py
+++ src/main.py
@@ -1,16 +1,14 @@
 import sys
-from pathlib import Path
 import arrow
-import shutil
 import logging
-
 import os
-import re
 
-from typing import TYPE_CHECKING, List, Optional
+from pydub import AudioSegment
+from pathlib import Path
+from typing import List
 
 logging.basicConfig(
-    format="%(asctime)s - %(message)s", datefmt="%d-%b-%y %H:%M:%S", level=logging.DEBUG
+    format="%(asctime)s - %(message)s", datefmt="%d-%b-%y %H:%M:%S", level=logging.INFO
 )
 
 dry_run = "--dry-run" in sys.argv
@@ -20,23 +18,28 @@ class AudioFile:
     supported_types = {"wav", "mp3", "m4a"}
 
     def __init__(self, path: str, dest_dir: str, artist="Isaac Thiessen"):
-        modified_time = arrow.get(os.path.getmtime(path))
+        modified_time = arrow.get(os.path.getctime(path))
+
         self.name = Path(path).name
-        self.extension = os.path.splitext(path)[1]
+        # [1:] removes leading period
+        self.extension = os.path.splitext(path)[1][1:]
 
         self.year = str(modified_time.year)
         self.month = modified_time.format("MMMM")
 
-        self.title = Path(path).stem
-        self.album = f"{self.year}-{self.month}"
-        self.artist = artist
+        self.tags = {
+            "title": Path(path).stem,
+            "album": f"{self.year}-{self.month}",
+            "artist": artist,
+            "year": self.year,
+            "month": self.month,
+        }
 
         self.src = path
         self.dest_directory = os.path.join(dest_dir, self.year, self.month)
-        self.dest = os.path.join(self.dest_directory, self.name)
+        self.dest = os.path.join(self.dest_directory, self.tags["title"] + ".mp3")
 
-        # [1:] removes leading period
-        self.supported = self.extension[1:] in self.supported_types
+        self.supported = self.extension in self.supported_types
 
 
 def find_audio_files(src: str, dest: str) -> "List[AudioFile]":
@@ -60,21 +63,26 @@ def ensure_path(file: AudioFile):
         pass
 
 
-def move_file(file: AudioFile) -> None:
+def copy_file(file: AudioFile) -> None:
     ensure_path(file)
 
     logging.info(
-        f'{"DRY_RUN -- " if dry_run else ""}Moving: "{file.src}" to "{file.dest}"'
+        f'{"DRY_RUN -- " if dry_run else ""}Converting: "{file.src}" to "{file.dest}"'
     )
     if not dry_run:
-        shutil.move(file.src, file.dest)
+        sound = AudioSegment.from_file(file.src, format=file.extension)
+        sound.export(file.dest, format="mp3", tags=file.tags)
 
 
 def main(src: str, dest: str) -> None:
     audio_files = find_audio_files(src, dest)
 
     for file in audio_files:
-        move_file(file)
+        if os.path.exists(file.dest):
+            logging.info(f"Skipping {file.dest}")
+            continue
+
+        copy_file(file)
 
 
 if __name__ == "__main__":
